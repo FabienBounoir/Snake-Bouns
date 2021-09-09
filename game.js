b46a08b4959b4d9fb27817fcf7c09ac8101b96fd
fabienbounoir <fabien.bounoir@epitech.eu>
2021-09-09 18:52:25 +0200

    finalisation game Snake

--- game.js
+++ game.js
@@ -1,10 +1,22 @@
-import { update as updateSnake , drawn as drawSnake, SNAKE_SPEED } from './snake'
+import { update as updateSnake , draw as drawSnake, getSnakeHead, snakeIntersection, SNAKE_SPEED } from './snake.js'
+import { update as updateFood, draw as drawFood } from './food.js'
+import { outsideGrid } from './grid.js'
 
 let lastRenderTime = 0
+let gameOver = false
 const gameBoard = document.getElementById('game-board')
 
 function main(currentTime)
 {
+    if(gameOver)
+    {
+        if(confirm("Perdu. clique sur 'OK' pour rejouer"))
+        {
+            window.location = '/'
+        }
+        return 
+    }
+
     window.requestAnimationFrame(main);
     const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
     if(secondsSinceLastRender < 1 / SNAKE_SPEED) return
@@ -21,9 +33,18 @@ window.requestAnimationFrame(main);
 function update() 
 {
     updateSnake()
+    updateFood()
+    checkDeath()
 }
 
 function draw() 
 {  
+    gameBoard.innerHTML = "";
     drawSnake(gameBoard)
+    drawFood(gameBoard)
+}
+
+function checkDeath()
+{
+    gameOver =  outsideGrid(getSnakeHead()) || snakeIntersection()
 }
